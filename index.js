d7a627753a8b2e64c872e1c3356f3ecde2260458
dhaval-coalition <dhaval.mandaliya@coalitiontechnologies.com>
2026-09-09 19:08:13 +0530

    updated the code as per the requirement

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config({ quiet: true });
+
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -7,64 +9,60 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+// The token remains in .env, which is ignored by Git.
+const privateAppAccess = process.env.HUBSPOT_ACCESS_TOKEN || process.env.PRIVATE_APP_TOKEN;
+const customObjectType = process.env.HUBSPOT_CUSTOM_OBJECT_TYPE || process.env.CUSTOM_OBJECT_TYPE_ID;
+const customObjectUrl = `https://api.hubapi.com/crm/v3/objects/${customObjectType}`;
+const headers = {
+    Authorization: `Bearer ${privateAppAccess}`,
+    'Content-Type': 'application/json'
+};
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
+// Route 1: Retrieve Video Game custom-object records for the homepage table.
+app.get('/', async (req, res) => {
     try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+        const response = await axios.get(customObjectUrl, {
+            headers,
+            params: {
+                properties: 'name,publisher,price'
+            }
+        });
+
+        res.render('homepage', {
+            title: 'Custom Object Table | Integrating With HubSpot I Practicum',
+            data: response.data.results
+        });
     } catch (error) {
-        console.error(error);
+        console.error(error.response?.data || error.message);
+        res.status(500).send('Unable to retrieve Video Game records from HubSpot.');
     }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
+// Route 2: Render the Pug form for creating a new Video Game record.
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', {
+        title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'
+    });
+});
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
+// Route 3: Send the three form values to HubSpot, then return to the homepage.
+app.post('/update-cobj', async (req, res) => {
+    const newVideoGame = {
+        properties: {
+            name: req.body.name,
+            publisher: req.body.publisher,
+            price: req.body.price
+        }
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+    try {
+        await axios.post(customObjectUrl, newVideoGame, { headers });
+        res.redirect('/');
+    } catch (error) {
+        console.error(error.response?.data || error.message);
+        res.status(500).send('Unable to create the Video Game record in HubSpot.');
     }
-
 });
-*/
 
 
 // * Localhost
